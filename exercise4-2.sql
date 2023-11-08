6ab5822b62324d5a11e7fe2ebeb31ca748627b17
JY0723 <73513896+JY0723@users.noreply.github.com>
2023-11-08 23:22:45 +0900

    update exercise 4-2.sql

--- exercise4-2.sql
+++ exercise4-2.sql
@@ -1,54 +1,58 @@
 CREATE TABLE `users` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(10) NOT NULL,
-    `password` VARCHAR(20) NOT NULL,
-    `nickname` VARCHAR(10) NOT NULL,
-    `profile_img_link` VARCHAR(256) NOT NULL,
+    `username` VARCHAR(16) NOT NULL,
+    `password` VARCHAR(64) NOT NULL,
+    `display_name` VARCHAR(16) NOT NULL,
+    `profile_image` VARCHAR(128) NOT NULL,
     `profile_message` VARCHAR(128) NOT NULL,
-    `is_out` TINYINT(1) NOT NULL DEFAULT 0,
-    `join_date` DATETIME NOT NULL, 
-    PRIMARY KEY(`id`)
+    `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
+    `date_joined` DATETIME NOT NULL,
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `channels` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(32) NOT NULL
-    `maker` INT NOT NULL,
-    `link` VARCHAR(256) NOT NULL,
-    `limit_people` INT NOT NULL,
-    `is_out` TINYINT(1) NOT NULL DEFAULT 0,
-    `make_date` DATETIME NOT NULL,
-    PRIMARY KEY(`id`),
-    FOREIGN KEY(`maker`) REFERENCES `users`(`id`) ON DELETE CASCADE
+    `name` VARCHAR(32) NOT NULL,
+    `created_by` INT NOT NULL,
+    `channel_link` VARCHAR(128) NOT NULL,
+    `capacity` INT NOT NULL,
+    `is_deleted` TINYINT(1) NOT NULL DEFAULT 0,
+    `created_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `chats` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `message` TEXT NOT NULL,
-    `maker` INT NOT NULL,
+    `created_by` INT NOT NULL,
     `channel` INT NOT NULL,
-    `make_date` DATETIME NOT NULL,
-    PRIMARY KEY(`id`),
-    FOREIGN KEY(`maker`) REFERENCES `users`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY(`channel`) REFERENCES `channels`(`id`) ON DELETE CASCADE
+    `created_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`channel`) REFERENCES `channels`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `blocks` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `blocked` INT NOT NULL,
-    `block` INT NOT NULL,
-    `block_date` DATETIME NOT NULL,
-    PRIMARY KEY(`id`),
-    FOREIGN KEY(`blocked`) REFERENCES `users`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY(`block`) REFERENCES `users`(`id`) ON DELETE CASCADE
+    `blocked_by` INT NOT NULL,
+    `blocked_user` INT NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`blocked_by`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`blocked_user`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 CREATE TABLE `follows` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `follower` INT NOT NULL,
     `followee` INT NOT NULL,
-    `follow_date` DATETIME NOT NULL,
-    PRIMARY KEY(`id`),
-    FOREIGN KEY(`follower`) REFERENCES `users`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY(`followee`) REFERENCES `users`(`id`) ON DELETE CASCADE
+    `created_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`follower`) REFERENCES `users`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`follower`) REFERENCES `users`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
